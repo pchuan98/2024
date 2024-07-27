00e5f4f49ba83c2031ed21acfa4dbe3545ff8fdb
chuan <haeer98@outlook.com>
2024-07-28 02:15:22 +0800

    feat: combine test about pmos and nmos

--- arch/study/1.v
+++ arch/study/1.v
@@ -9,31 +9,8 @@
  * 
  * gate = 0 ==> pmos is on
  * gate = 1 ==> pmos is off
- */
-module TEST1 ();
-    reg gate, source;
-    wire drain;
-
-    pmos p1 (drain, source, gate);
-
-    initial begin
-        $display("Time\tSource\tGate\tDrain");
-        $monitor("%g\t\t%b\t\t%b\t\t%b", $time, source, gate, drain);
-
-        // init
-        gate   = 0;
-        source = 0;
-
-        #10 source = 1;
-
-        #10;
-        gate   = 1;
-        source = 0;
-        #10 source = 1;
-    end
-endmodule
-
-/**
+ * ==============================
+ * ==============================
  *            Gate
  * Source -----------> Drain
  * nmos (Drain, Source, Gate);
@@ -41,15 +18,16 @@ endmodule
  * gate = 0 ==> nmos is off
  * gate = 1 ==> nmos is on
  */
-module TEST2 ();
+
+module TEST1 ();
     reg gate, source;
-    wire drain;
+    wire drain_p, drain_n;
 
-    nmos n1 (drain, source, gate);
-
+    pmos p1 (drain_p, source, gate);
+    nmos n1 (drain_n, source, gate);
     initial begin
-        $display("Time\tSource\tGate\tDrain");
-        $monitor("%g\t\t%b\t\t%b\t\t%b", $time, source, gate, drain);
+        $display("Time\tSource\tGate\tPMOS\tNMOS");
+        $monitor("%g\t\t%b\t\t%b\t\t%b\t\t%b", $time, source, gate, drain_p, drain_n);
 
         // init
         gate   = 0;
@@ -63,3 +41,4 @@ module TEST2 ();
         #10 source = 1;
     end
 endmodule
+
